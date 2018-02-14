f68bd370e39dff770abbdd8c1a0394d4c638adf6
ryotaWindows <ryota.ono@gmail.com>
2018-02-14 12:35:59 +0700

    change words. 広告宣伝 -> マーケティング
    Signed-off-by: ryotaWindows <ryota.ono@gmail.com>

--- nanj/resources/lang/ja/message.php
+++ nanj/resources/lang/ja/message.php
@@ -29,7 +29,7 @@ return [
     'preSeason' => 'オープン戦',
     'operation' => '運営チーム',
     'lockUp' => 'ロックアップ',
-    'advertisementFee' => '広告宣伝活動費',
+    'advertisementFee' => 'マーケティング活動費',
     'nanjMean' => '<strong>N</strong>EXT-GENERATION <strong>A</strong>THLETE <strong>N</strong>ETWORK <strong>J</strong>OINT',
     'nanjMean2' => '<strong>E</strong>THEREUM <strong>E</strong>RC223 <strong>T</strong>OKEN <strong>F</strong>ULLY <strong>C</strong>OMPATIBLE <strong>W</strong>ITH <strong>E</strong>RC20 <strong>T</strong>OKEN <strong>S</strong>TANDARD',
     'assignDescription' =>
