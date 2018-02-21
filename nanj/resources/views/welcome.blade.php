2f5ac7eceec99fee0fda314c9d667d314b8a8c2d
Nekoeri88 <35603584+Nekoeri88@users.noreply.github.com>
2018-02-21 17:34:38 +0900

    remove top button in navigation (#13)

--- nanj/resources/views/welcome.blade.php
+++ nanj/resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
                 <div class="col-md-10 col-sm-7 col-9">
                     <h1><img src="/img/header_img_01.png" alt="NANJ"><span>{{__('message.coinName')}}</span></h1>
                     <ul class="nav d-none d-md-block">
-                        <li class="btn_top">{{__('message.top')}}</li>
+                        {{-- <li class="btn_top">{{__('message.top')}}</li> --}}
                         <li class="btn_nav"><a href="#about">{{__('message.about')}}</a></li>
                         <li class="btn_nav"><a href="#roadmap">{{__('message.roadMap')}}</a></li>
                         <li class="btn_nav"><a href="#group01">{{__('message.whitePaper')}}</a></li>
